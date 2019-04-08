0387dbc19ed3bb7c8dafd16c5cd00b349ed51d7c
Eleftherios Avramidis <el.avramidis@gmail.com>
2019-04-08 12:13:23 +0100

    Changed arguments position

--- batch_submit.sh
+++ batch_submit.sh
@@ -7,6 +7,6 @@ for sess in `seq 1 1 2` ; do
     echo 'working on:' "${sess}"
     
     mkdir ${baseDir}
-	sbatch --output=paok${sess}.log submit.slurm ${baseDir}
+	sbatch submit.slurm ${baseDir} --output=paok${sess}.log
 
 done
